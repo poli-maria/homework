9ab664adaf8987531fcf30bdf1c23c7dabd58de0
poli-maria <molokotyan@gmail.com>
2024-09-20 13:03:56 +0300

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -3,11 +3,11 @@ print(name[0])
 print(name[-1])
 print(name[5::])
 print(name[::-1])
-print(name[::-2])
+print(name[1::2])
 
 name = 'Солнце'
 print(name[0])
 print(name[-1])
 print(name[3::])
 print(name[::-1])
-print(name[::2])
+print(name[1::2])
