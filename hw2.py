ef156a3a16e0323f5f244d6ddbe6448125e34348
poli-maria <molokotyan@gmail.com>
2024-09-23 00:25:12 +0300

    Add files via upload

--- hw2.py
+++ hw2.py
@@ -1,5 +1,5 @@
 homework = 12
 total_hours = 1.5
 course_name = 'Python'
-time_for_a_single_task = (1.5 / 12)
-print('Курс:', course_name,',','всего задач:', homework,',','затрачено часов:', total_hours,',','среднее время выполнения:', time_for_a_single_task,'.')
+
+print('Курс:', course_name,',','всего задач:', homework,',','затрачено часов:', total_hours,',','среднее время выполнения:', (total_hours / homework),'.')
