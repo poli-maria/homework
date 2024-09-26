4c768f3b82fbfe3422c198d3b53d9e76149b60ea
poli-maria <molokotyan@gmail.com>
2024-09-26 22:43:40 +0300

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,16 @@
+my_dict = {'Sarah': 1998, 'Mike': 1995, 'Isabella': 2001, 'Peter': 2004}
+print(my_dict)
+print(my_dict.get('Isabella'))
+print(my_dict.get('Timmy'))
+my_dict.update({'Ivan': 1993, 'Oliver': 1985})
+a = my_dict.pop('Peter')
+print(a)
+print(my_dict)
+
+my_set = {1, 2, 3, 'Eggplant', True, 1.5}
+print(my_set)
+my_set.add(7), (85)
+my_set.remove('Eggplant')
+print(my_set)
+
+
